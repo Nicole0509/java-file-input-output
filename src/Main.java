e6d4473efde3dcae3c66a2a3192920e6283cfe42
Nicole0509 <niashavril0509@gmail.com>
2025-09-22 15:22:44 +0200

    Succesfully added a name to the file

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Student Report System With Java Streams!");
+        System.out.print("Enter the student's name:");
+        String name = scanner.nextLine();
 
         try{
             BufferedWriter writer = new BufferedWriter(new FileWriter("writtingExample.txt"));
-            writer.write("Hello World!");
+            writer.write(name);
             writer.close();
         }catch(Exception e){
             System.out.println("Something went wrong!");
@@ -30,7 +35,6 @@ public class Main {
             System.out.println("Something went wrong!");
         }
 
-
-
+        scanner.close();
     }
 }
