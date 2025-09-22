4ca26f626131dec5bb1347ab6043b055f175080a
Nicole0509 <niashavril0509@gmail.com>
2025-09-22 15:11:20 +0200

    Successfully wrote hello world into a file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Student Report System With Java Streams!");
+
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("writtingExample.txt"));
+            writer.write("Hello World!");
+            writer.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+
 
     }
 }
