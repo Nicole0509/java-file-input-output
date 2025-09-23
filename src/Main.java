88b176184408a29a98e94626426af44549f6bdd1
Nicole0509 <niashavril0509@gmail.com>
2025-09-23 21:51:25 +0200

    Successfully ready a text file using a File Input Stream class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,67 +8,67 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int rollNumber;
-        double marks;
-        String name;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Student Report System With Java Streams!");
+//        int rollNumber;
+//        double marks;
+//        String name;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Student Report System With Java Streams!");
 
         // Inserting Name
-        do{
-            System.out.print("Enter the student's name:");
-            name = scanner.nextLine().trim();
+//        do{
+//            System.out.print("Enter the student's name:");
+//            name = scanner.nextLine().trim();
+//
+//            if(name.isEmpty()) {
+//                System.out.println("Item name cannot be empty. Please enter a name.");
+//            }
+//        } while(name.trim().isEmpty());
+//
+//        // Inserting roll Number
+//        do{
+//            System.out.print("Enter the student's roll number: ");
+//            try {
+//                rollNumber = scanner.nextInt();
+//                scanner.nextLine();
+//                break;
+//            } catch (InputMismatchException e) {
+//                System.out.println("Invalid Input. Please student's roll number: ");
+//                scanner.nextLine();
+//            }
+//        } while(true);
+//
+//        // Inserting marks
+//        do{
+//            System.out.print("Enter the student's marks: ");
+//            try {
+//                marks = scanner.nextDouble();
+//                scanner.nextLine();
+//                break;
+//            } catch (InputMismatchException e) {
+//                System.out.println("Invalid Input. Please student's marks: ");
+//                scanner.nextLine();
+//            }
+//        } while(true);
+//
+//        System.out.println();
 
-            if(name.isEmpty()) {
-                System.out.println("Item name cannot be empty. Please enter a name.");
-            }
-        } while(name.trim().isEmpty());
-
-        // Inserting roll Number
-        do{
-            System.out.print("Enter the student's roll number: ");
-            try {
-                rollNumber = scanner.nextInt();
-                scanner.nextLine();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid Input. Please student's roll number: ");
-                scanner.nextLine();
-            }
-        } while(true);
-
-        // Inserting marks
-        do{
-            System.out.print("Enter the student's marks: ");
-            try {
-                marks = scanner.nextDouble();
-                scanner.nextLine();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid Input. Please student's marks: ");
-                scanner.nextLine();
-            }
-        } while(true);
-
-        System.out.println();
-
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter("writtingExample.txt", true));
-            writer.write(name + "\t" + rollNumber + "\t" + marks + "\n" );
-            writer.close();
-        }catch(Exception e){
-            System.out.println("Something went wrong!");
-        }
+//        try{
+//            BufferedWriter writer = new BufferedWriter(new FileWriter("writtingExample.txt", true));
+//            writer.write(name + "\t" + rollNumber + "\t" + marks + "\n" );
+//            writer.close();
+//        }catch(Exception e){
+//            System.out.println("Something went wrong!");
+//        }
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("writtingExample.txt"));
+            FileInputStream reader = new FileInputStream("writtingExample.txt");
 
-            String line;
+            int i;
 
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+            while((i = reader.read()) != -1){
+                System.out.print((char)i);
             }
 
             reader.close();
@@ -76,6 +76,6 @@ public class Main {
             System.out.println("Something went wrong!");
         }
 
-        scanner.close();
+//        scanner.close();
     }
 }
